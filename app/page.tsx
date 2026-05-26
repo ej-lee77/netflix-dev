e3298051bc12a9dd436ff7a3422acaf10d7218ab
wldbs2334 <wldbs2334@gmail.com>
2026-05-26 16:37:29 +0900

    메인- 추천섹션 일단올림

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import MovieCarousel from "@/components/main/MovieCarousel";
 import MovieList from "@/components/MovieList";
 import TvList from "@/components/TvList";
 import { useMovieStore } from "@/store/useMovieStore";
@@ -6,14 +7,14 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 export default function Home() {
-  const {onFetchPopular, onFetchTvs} = useMovieStore();
-  useEffect(()=>{
+  const { onFetchPopular, onFetchTvs } = useMovieStore();
+  useEffect(() => {
     onFetchPopular();
     onFetchTvs();
   }, []);
   return (
     <div className="inner">
-
+      <MovieCarousel />
       <MovieList />
       <TvList />
     </div>
