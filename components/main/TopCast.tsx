5f4165cb0ac6b48631591fda630ea821df1c7d10
youngseo-claire <claireyslahto@gmail.com>
2026-05-26 18:34:38 +0900

    공통사항 scss사용

--- components/main/TopCast.tsx
+++ components/main/TopCast.tsx
@@ -1,10 +1,6 @@
 "use client";
 import React, { useEffect } from 'react';
 import { useMovieStore } from '@/store/useMovieStore';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
 import './scss/topCast.scss';
 
 interface Props {
@@ -17,8 +13,7 @@ interface Props {
  * - 부모(RecommendList)로부터 현재 가운데 작품의 id와 mediaType을 받아서
  *   해당 작품의 출연진을 가져옴
  * - 둥근 프로필 + 배우명 + 배역명
- * - 부모 풀폭 배경(진한 남색)을 위해 .top-cast-bg 가 바깥을 감싸고
- *   안쪽 컨텐츠만 .inner 로 1600px 정렬
+ * - 10명 카드를 grid 로 한 줄에 균등 배치 (Swiper 없음, 스크롤 없음)
  */
 export default function TopCast({ id, mediaType }: Props) {
   const { casts, onFetchCredits } = useMovieStore();
@@ -49,33 +44,24 @@ export default function TopCast({ id, mediaType }: Props) {
           {topCasts.length === 0 ? (
             <p className="cast-empty">출연진 정보를 불러오는 중입니다.</p>
           ) : (
-            <Swiper
-              modules={[Navigation]}
-              navigation
-              slidesPerView={10}
-              spaceBetween={20}
-              slidesPerGroup={5}
-              className="cast-swiper"
-            >
+            <ul className="cast-grid">
               {topCasts.map((cast) => (
-                <SwiperSlide key={cast.id}>
-                  <div className="cast-card">
-                    <div className="cast-photo">
-                      {cast.profile_path ? (
-                        <img
-                          src={`https://image.tmdb.org/t/p/w300${cast.profile_path}`}
-                          alt={cast.name}
-                        />
-                      ) : (
-                        <div className="photo-empty">{cast.name.charAt(0)}</div>
-                      )}
-                    </div>
-                    <p className="cast-name">{cast.name}</p>
-                    <p className="cast-character">{cast.character || '-'}</p>
+                <li key={cast.id} className="cast-card">
+                  <div className="cast-photo">
+                    {cast.profile_path ? (
+                      <img
+                        src={`https://image.tmdb.org/t/p/w300${cast.profile_path}`}
+                        alt={cast.name}
+                      />
+                    ) : (
+                      <div className="photo-empty">{cast.name.charAt(0)}</div>
+                    )}
                   </div>
-                </SwiperSlide>
+                  <p className="cast-name">{cast.name}</p>
+                  <p className="cast-character">{cast.character || '-'}</p>
+                </li>
               ))}
-            </Swiper>
+            </ul>
           )}
         </section>
       </div>
