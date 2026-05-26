2a7739d349c7ac727dd40958ebb88c7f323a1672
wldbs2334 <wldbs2334@gmail.com>
2026-05-26 19:46:21 +0900

    그리드에 포스터 뿌리기

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
+  allowedDevOrigins: ["172.16.54.18"],
 };
-
-// next.config.js
-module.exports = {
-  allowedDevOrigins: ['172.16.54.18'],
-}
 
 export default nextConfig;
