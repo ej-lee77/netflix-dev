0180288c50a80435a721afa7a9488ac3bc91c730
yejun123188 <yejun123188@gmail.com>
2026-05-27 16:06:07 +0900

    오류수정

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -1,10 +1,12 @@
 import ConnectBestReviewHero from "@/components/connect/ConnectBestReviewHero";
+import ConnectReviewSpotlight from "@/components/connect/ConnectReviewSpotlight";
 
 export default function ConnectPage() {
   return (
     <div className="sub-page">
       <div className="w-full">
         <ConnectBestReviewHero />
+        <ConnectReviewSpotlight />
       </div>
     </div>
   );
